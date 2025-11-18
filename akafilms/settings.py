f99f2023270361c49c2e89ab4ce996390b2c54ac
Karla Clemente <kclemenh@ciencias.unam.mx>
2025-11-18 13:57:59 -0600

    Se agregaron variables de entorno

--- akafilms/settings.py
+++ akafilms/settings.py
@@ -4,17 +4,18 @@ Django settings for akafilms project.
 
 from pathlib import Path
 import os
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aka-films-2024-change-in-production'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 INSTALLED_APPS = [
